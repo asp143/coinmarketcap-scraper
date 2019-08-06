b9102c3a5fcc6c3dfe903b99b992ca479000bee7
asp143 <ralphmungcal@gmail.com>
2019-08-06 20:55:21 +0800

    Added new constants

--- constants.js
+++ constants.js
@@ -1,5 +1,19 @@
-const coins = ['ripple'];
+// External packages
+const moment = require('moment');
+
+
+const COINS = ['ripple'];
+const DATA_PROPERTIES = ['date', 'open_price', 'high', 'low','close_price', 'volume', 'market_cap'];
+const PREFIX_URL = 'https://coinmarketcap.com';
+
+const YEAR_TO_DATE_URL = `?start=${moment().startOf('year').format('YYYYMMDD')}&end=${moment().format('YYYYMMDD')}`;
+
+const BASE_FILE_NAME = `${Date.now()}.json`;
 
 module.exports = {
-    coins
+    COINS,
+    DATA_PROPERTIES,
+    PREFIX_URL,
+    YEAR_TO_DATE_URL,
+    BASE_FILE_NAME
 };
